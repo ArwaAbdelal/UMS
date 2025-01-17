d295e90fb7e513f2d4c7f0e0941de8501d7d8dcd
ArwaAbdelal <arwaabdelall1@gmail.com>
2025-01-17 21:03:30 +0200

    register+login

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UserModel from "../../../DB/model/user.model.js";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 const router=Router();
 
 
@@ -20,19 +21,20 @@ router.post('/register',async(req,res)=>{
 
 router.post('/login',async(req,res)=>{
     const {email,password}= req.body;
-    const checkUser= await UserModel.findOne({
+    const user= await UserModel.findOne({
         where:{
             email:email
         }
     });
-    if (checkUser== null){
+    if (user== null){
         return res.status(404).json({message:"invalid email"});
     };
-    const checkpassword = await bcrypt.compareSync(password, checkUser.hashedPassword);
+    const checkpassword = await bcrypt.compareSync(password, user.password);
     if (checkpassword==false){
         return res.status(400).json({message:"invalid password"});
     };
-    return res.status(200).json({message:"Success", checkUser});
+    const token = jwt.sign({ id: user.id, name: user.userName}, 'ar78wa');
+    return res.status(200).json({message:"Success", token});
 });
 
 
